212484998ce3af036f6003066c0bffa211b06454
bharvi-bissa <bharvi.bissa@gmail.com>
2017-12-06 22:33:44 +0530

    Update editprofile.php

--- editprofile.php
+++ editprofile.php
@@ -1,11 +1,6 @@
 <?php
   
-  define('DB_SERVER', '127.0.0.1');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '');
-  define('DB_DATABASE', 'eduportal');
-  
-  $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die(mysqli_error());
+  require "connections.php";
   $error="";
   session_start();
   $uname=$_SESSION['user_session'];
