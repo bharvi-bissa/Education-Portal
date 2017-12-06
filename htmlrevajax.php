11392a444ab1f427a8810fc8efecc0dfaac4f91a
bharvi-bissa <bharvi.bissa@gmail.com>
2017-12-06 22:34:42 +0530

    Update htmlrevajax.php

--- htmlrevajax.php
+++ htmlrevajax.php
@@ -1,11 +1,6 @@
 <?php
 	session_start();
-	define('DB_SERVER', '127.0.0.1');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_DATABASE', 'eduportal');
-	$uname="";
-	$db = @mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die(mysqli_error());
+	require "connections.php";
 	
 	$uname=$_SESSION['user_session'];
 	$course="html";
